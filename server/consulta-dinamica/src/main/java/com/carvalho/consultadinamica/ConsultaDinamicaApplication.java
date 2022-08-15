d4993873afb42de928e3455a762e9195c8242011
João Carvalho <jpaulofranca10@gmail.com>
2022-08-15 17:36:49 -0300

    commit inicial - criando projeto

---
+++ server/consulta-dinamica/src/main/java/com/carvalho/consultadinamica/ConsultaDinamicaApplication.java
@@ -0,0 +1,13 @@
+package com.carvalho.consultadinamica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaDinamicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaDinamicaApplication.class, args);
+	}
+
+}
---
+++ server/consulta-dinamica/src/test/java/com/carvalho/consultadinamica/ConsultaDinamicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvalho.consultadinamica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaDinamicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
